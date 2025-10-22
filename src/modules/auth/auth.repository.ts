ab93f3a287788825d5ff51c6e523f0953044a231
amuroi777 <muroiaki7722@gmail.com>
2025-10-22 21:05:31 +0900

    ログインの永続化

--- src/modules/auth/auth.repository.ts
+++ src/modules/auth/auth.repository.ts
@@ -1,8 +1,8 @@
-import api from "../../lib /api";
+import api from "../../lib/api";
 import { User } from "../users/user.entity";
 
 export const authRepository = {
-  async Signup(name: string, email: string, password: string): Promise<{ user: User; token: string }> {
+  async signup(name: string, email: string, password: string): Promise<{ user: User; token: string }> {
     const result = await api.post("/auth/signup", {
       name,
       email,
@@ -10,5 +10,16 @@ export const authRepository = {
     });
     const { user, token } = result.data;
     return { user: new User(user), token };
+  },
+  async signin(email: string, password: string) {
+    const result = await api.post("/auth/signin", { email, password });
+    const { user, token } = result.data;
+    return { user: new User(user), token };
+  },
+  async getCurrentUser(): Promise<User | undefined> {
+    const result = await api.get("/auth/me");
+    if (result.data == null) return undefined;
+
+    return new User(result.data);
   },
 };
