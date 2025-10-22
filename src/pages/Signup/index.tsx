74df044e33a4e419c874d2e128b128bdc3bd6c30
amuroi777 <muroiaki7722@gmail.com>
2025-10-22 21:04:08 +0900

    サインアップの修正

--- src/pages/Signup/index.tsx
+++ src/pages/Signup/index.tsx
@@ -1,17 +1,23 @@
-import { Link } from "react-router-dom";
+import { Link, Navigate } from "react-router-dom";
 import "./auth.css";
 import { useState } from "react";
 import { authRepository } from "../../modules/auth/auth.repository";
+import { useCurrentUseStore } from "../../modules/auth/current-user.state";
 
 function Signup() {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const { currentUser, setCurrentUser } = useCurrentUseStore();
 
   const signup = async () => {
     if (name == "" || email == "" || password == "") return;
     const { user, token } = await authRepository.signup(name, email, password);
+    localStorage.setItem("token", token);
+    setCurrentUser(user);
   };
+
+  if (currentUser != null) return <Navigate to="/" />;
 
   return (
     <div className="signup-container">
