8f21e5673f307c4724780ed1f6fd0eb69d3a0c61
muroi <amuro683@gmail.com>
2025-12-10 21:03:06 +0900

    ui周りの動作修正

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -3,18 +3,49 @@ import "./Home.css";
 import Sidebar from "./Sidebar";
 import MainContent from "./MainContent";
 import { useCurrentUseStore } from "../../modules/auth/current-user.state";
-import { Navigate } from "react-router-dom";
+import { Navigate, useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { Workspace } from "../../modules/workspaces/workspace.entity";
+import { workspaceRepository } from "../../modules/workspaces/workspace.repository";
 
 function Home() {
   const { currentUser } = useCurrentUseStore();
+  const [workspaces, setWorkspaces] = useState<Workspace[]>([]);
+  const params = useParams();
+  const { workspaceId } = params;
+  const selectedWorkspace = workspaces.find((workspace) => workspace.id == workspaceId)
+
+  useEffect(() => {
+    fetchWorkspaces()
+  }, [])
+
+  const fetchWorkspaces = async () => {
+    try {
+      const workspaces = await workspaceRepository.find()
+      setWorkspaces(workspaces);
+    }catch (error) {
+      console.log('ワークスペースの作成に失敗しました', error)
+    }
+  }
+
   if (currentUser == null) return <Navigate to="/signin" />;
 
   return (
     <div className="slack-container">
-      <WorkspaceSelector />
+      <WorkspaceSelector
+        workspaces={workspaces}
+        setWorkspaces={setWorkspaces}
+        selectedWorkspaceId={workspaceId!}
+      />
       <>
-        <Sidebar />
-        <MainContent />
+      {selectedWorkspace != null ? (
+        <>
+         <Sidebar selectedWorkspace={selectedWorkspace} />
+         <MainContent />
+        </>
+      ) : (
+        <div className="sidebar" />
+      )}
       </>
     </div>
   );
